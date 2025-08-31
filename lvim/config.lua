7fa0be44cb8e12c75db04eba8abfc628945ba46a
ch-m-n <chaungo7703@gmail.com>
2025-08-31 13:43:56 +0700

    update

--- lvim/config.lua
+++ lvim/config.lua
@@ -3,7 +3,7 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-lvim.colorscheme = "retrobox"
+lvim.colorscheme = "murphy"
 -- lvim.builtin.lualine.style = "default"
 
 local components = require("lvim.core.lualine.components")
@@ -26,5 +26,5 @@ lvim.plugins = {
     end
   },
   { "Exafunction/codeium.vim", event = "BufEnter" },
-  -- { "foxbunny/vim-amber",      event = "BufEnter", config = function() vim.cmd("colorscheme amber") end },
+  { "foxbunny/vim-amber",      event = "BufEnter", config = function() vim.cmd("colorscheme amber") end },
 }
